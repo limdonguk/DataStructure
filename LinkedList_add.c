201971c6d3f37f10fcea55b8ba1311390855a141
LimDongWook <donguy94@naver.com>
2018-07-06 23:11:11 +0900

    Spacc 간격 변경

--- LinkedList_add.c
+++ LinkedList_add.c
@@ -4,82 +4,82 @@
 
 typedef struct LinkedListNodeType
 {
-	double data;
-	struct LinkedListNodeType *pLink;
+  double data;
+  struct LinkedListNodeType *pLink;
 } LinkedListNode;
 
 typedef struct LinkedListType
 {
-	int currentCount;
-	LinkedListNode headerNode;
+  int currentCount;
+  LinkedListNode headerNode;
 } LinkedList;
 
 LinkedList *createLinkedList()
 {
-	LinkedList *pReturn = (LinkedList *)malloc(sizeof(LinkedList));
-	memset(pReturn, 0, sizeof(LinkedList));
-	return pReturn;
+  LinkedList *pReturn = (LinkedList *)malloc(sizeof(LinkedList));
+  memset(pReturn, 0, sizeof(LinkedList));
+  return pReturn;
 }
 
 void addLinkedListData(LinkedList *pList, double data)
 {
-	LinkedListNode *pPreNode = NULL;
-	LinkedListNode *pNewNode = NULL;
+  LinkedListNode *pPreNode = NULL;
+  LinkedListNode *pNewNode = NULL;
 
-	pPreNode = &(pList->headerNode);
-	while (pPreNode->pLink != NULL)
-	{
-		pPreNode = pPreNode->pLink;
-	}
+  pPreNode = &(pList->headerNode);
+  while (pPreNode->pLink != NULL)
+  {
+    pPreNode = pPreNode->pLink;
+  }
 
-	pNewNode = (LinkedListNode *)malloc(sizeof(LinkedListNode));
-	pNewNode->pLink = NULL;
-	pNewNode->data = data;
-	pPreNode->pLink = pNewNode;
+  pNewNode = (LinkedListNode *)malloc(sizeof(LinkedListNode));
+  pNewNode->pLink = NULL;
+  pNewNode->data = data;
+  pPreNode->pLink = pNewNode;
 
-	pList->currentCount++;
+  pList->currentCount++;
 }
 
 double sumLinkedListData(LinkedList *pList)
 {
-	double total = 0;
-	LinkedListNode *pNode = NULL;
+  double total = 0;
+  LinkedListNode *pNode = NULL;
 
-	pNode = &(pList->headerNode);
-	for (int i = 0; i <= pList->currentCount; i++)
-	{
-		total += pNode->data;
-		pNode = pNode->pLink;
-	}
+  pNode = &(pList->headerNode);
+  for (int i = 0; i <= pList->currentCount; i++)
+  {
+    total += pNode->data;
+    pNode = pNode->pLink;
+  }
 
-	return total / pList->currentCount;
+  return total / pList->currentCount;
 }
 
 int main(int argc, char *argv[])
 {
-	int i = 1;
-	double inputData = 0;
-	LinkedList *pList = NULL;
+  int i = 1;
+  double inputData = 0;
+  LinkedList *pList = NULL;
 
-	pList = createLinkedList();
+  pList = createLinkedList();
 
-	while (1)
-	{
-		printf("> %d번째 점수는? (-1을 입력하면 끝납니다.) : ", i);
-		scanf_s("%lf", &inputData);
+  while (1)
+  {
+    printf("> %d번째 점수는? (-1을 입력하면 끝납니다.) : ", i);
+    scanf_s("%lf", &inputData);
 
-		if (inputData == -1)
-		{
-			break;
-		}
-		else
-		{
-			addLinkedListData(pList, inputData);
-			i++;
-		}
-	}
+    if (inputData == -1)
+    {
+      break;
+    }
+    else
+    {
+      addLinkedListData(pList, inputData);
+      i++;
+    }
+  }
 
-	printf("%d명의 평균 : %f\n", pList->currentCount, sumLinkedListData(pList));
+  printf("%d명의 평균 : %f\n", pList->currentCount, sumLinkedListData(pList));
 
-	return 0;
+  return 0;
 }
--- test.c
+++ test.c
@@ -1,1 +1,137 @@
-adsasd
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ArrayListNodeType
+{
+    int data;
+} ArrayListNode;
+
+typedef struct ArrayListType
+{
+    int maxCount;     // 최대 크기
+    int currentCount; // 현재 크기
+    ArrayListNode *pData;
+} ArrayList;
+
+ArrayList createArrayList(int count)
+{
+    ArrayList *pReturn = NULL;
+    if (count > 0)
+    {
+        pReturn = (ArrayList *)malloc(sizeof(ArrayList));
+        if (pReturn != NULL)
+        {
+            if (pReturn->currentCount < pReturn->maxCount)
+            {
+                pReturn->maxCount = count;
+                pReturn->currentCount = 0;
+                pReturn->pData = NULL;
+            }
+            else
+            {
+                printf("인덱스 범위 초과");
+            }
+        }
+        else
+        {
+            printf("비어있는 리스트");
+        }
+    }
+    else
+    {
+        printf("최대 배열 크기는 0 이상");
+    }
+
+    pReturn->pData = (ArrayListNode *)malloc(sizeof(ArrayListNode) * count);
+    if (pReturn->pData != NULL)
+    {
+        memset(pReturn->pData, 0, sizeof(ArrayListNode) * count);
+    }
+    else
+    {
+        printf("메모리 할당 실패");
+        free(pReturn);
+    }
+}
+
+int addArrayListData(ArrayList *pList, int position, int data)
+{
+    int i = 0;
+    if (pList != NULL)
+    {
+        if (pList->currentCount < pList->maxCount)
+        {
+            if (position >= 0 && position <= pList->currentCount)
+            {
+                for (i = pList->currentCount - 1; i >= position; i--)
+                {
+                    pList->pData[i + 1] = pList->pData[i];
+                }
+                pList->pData[position].data = data;
+                pList->currentCount++;
+            }
+            else
+            {
+                printf("위치 인덱스 범위 초과");
+            }
+        }
+        else
+        {
+            printf("최대 크기 초과");
+        }
+    }
+    else
+    {
+        printf("비어있는 List");
+    }
+
+    return 0;
+}
+
+int removeArrayListData(ArrayList *pList, int position)
+{
+    if (pList != NULL)
+    {
+        if (position >= 0 && position <= pList->currentCount)
+        {
+            for (int i = position; i < pList->currentCount - 1; i++)
+            {
+                pList->pData[i] = pList->pData[i + 1];
+            }
+            pList->currentCount--;
+        }
+    }
+}
+
+int getArrayListData(ArrayList *pList, int position)
+{
+    if (pList != NULL)
+    {
+        if (position >= 0 && position < pList->currentCount)
+        {
+            return pList->pData[position].data;
+        }
+    }
+}
+
+void deleteArrayList(ArrayList *pList)
+{
+    if (pList != NULL)
+    {
+        if (pList->pData != NULL)
+        {
+            free(pList->pData);
+        }
+        free(pList);
+    }
+}
+
+int getArrayListLength(ArrayList *pList)
+{
+    if (pList != NULL)
+    {
+        return pList->currentCount;
+    }
+    return 0;
+}
