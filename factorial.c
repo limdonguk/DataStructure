12145dbc1f2b273d2231d81fb655441eb7545aa2
LimDongWook <donguy94@naver.com>
2018-07-15 23:33:20 +0900

    Add : main()

--- factorial.c
+++ factorial.c
@@ -12,3 +12,12 @@ int factorial(int n){
     return ret;
 }
 
+int main() {
+    int input_value = 3;
+    int result = 0;
+
+    result = factorial(input_value);
+    printf("%d! = %d\n",input_value, result);
+
+    return 0;
+}
